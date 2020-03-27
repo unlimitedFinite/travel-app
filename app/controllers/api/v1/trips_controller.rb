b83c9cfe417504f79dc30a153db2abe4b73e0885
Daniele Deltodesco <deaddread666@gmail.com>
2020-03-27 22:38:12 +0000

    controller and model created

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,13 +1,39 @@
+# Trips API controller
 class Api::V1::TripsController < ApplicationController
   def index
+    trip = Trip.all.order(created_at: :desc)
+    render json: trip
   end
 
   def create
+    trip = Trip.create(trip_params)
+    if trip
+      render json: trip
+    else
+      render json: trip.errors
+    end
   end
 
   def show
+    if trip
+      render json: trip
+    else
+      render json: trip.errors
+    end
   end
 
   def destroy
+    trip&.destroy
+    render json: {message: 'Recipe deleted'}
+  end
+
+  private
+
+  def trip_params
+    params.permit(:name, :trip_start, :trip_end)
+  end
+
+  def trip
+    @trip ||= Trip.find(params[:id])
   end
 end
